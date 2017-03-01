ae2e4da5a9de6e811470d84f6dc1e894e349b3c5
nayan-paul <nayan.j.paul@gmail.com>
2017-02-28 22:44:15 -0500

    28-feb-17

--- pattern3-analytics-process.py
+++ pattern3-analytics-process.py
@@ -12,11 +12,30 @@ import numpy as np
 #COMMON
 nlp = spacy.load('en')
 ICD_REGEX = re.compile(r'.*([A-Z].*)-.*')
+SPEC_REGEX = re.compile(r'([A-Z]\d+[\.]?\d*)\s+.*')
+
 def standardizeICD(val):
 	try:
 		regxLst = []
 		for tmp in val.split(')'):
 			match = re.findall(ICD_REGEX,tmp)
+			if match:
+				regxLst.append( match)
+			#if
+		#for	
+		regxLst.sort()
+		return  "~".join(regxLst)
+	#try
+	except:
+		traceback.print_exc()
+	#except
+#def
+
+def standardizeSpecAssesment(val):
+	try:
+		regxLst = []
+		for tmp in val.split(','):
+			match = re.findall(SPEC_REGEX,tmp)
 			if match:
 				regxLst.append( match[0])
 			#if
@@ -33,61 +52,44 @@ def standardizeICD(val):
 def  loadNas_Details_Data4mElasticSearch():
 	try:
 		query=json.dumps({
-		"size": 1000000,
-		"fields" :  ["ITEM", "STATUS","PROVIDER","NETWORKTYPE","BENEFICIARY","TREATMENTDATE","PROFESSIONAL","ITEMPAYERSHARE","CHRONIC","SPECASSESSMENT","SERVICE","MASTERCONTRACT","CLAIMTYPE"],
+		"size": 9000000,
+		"fields" :  ["PROVIDER","PROFESSIONAL","SPECASSESSMENT","ITEMPAYERSHARE","ITEM","TREATMENTDATE","CHRONIC"],
 		"query": {
 		"bool": {
 		"must": [
 		{"match": {"PROVIDERTYPE": "Pharmacy"}},
-		{ "match": { "FOB":"Out-Patient" }}
+		{ "match": { "FOB":"Out-Patient" }},
+		{ "match": { "STATUS":"Settled" }}
 		]} }
 		})
 		response = requests.post('http://localhost:9200/nas_details/nas_claim_details/_search?scroll=1m', data=query)
 		
 		jsonDoc = json.loads(response.text)
 		file = open('/opt/takaful/processed-data/pattern3-nas-input.csv','w')
-		file.write("ITEM~STATUS~PROVIDER~NETWORKTYPE~BENEFICIARY~TREATMENTDATE~PROFESSIONAL~ITEMPAYERSHARE~CHRONIC~SPECASSESSMENT~SERVICE~MASTERCONTRACT~CLAIMTYPE\n")
+		file.write("PROVIDER~PROFESSIONAL~SPECASSESSMENT~ITEMPAYERSHARE~ITEM~TREATMENTDATE~CHRONIC\n")
 		for obj in jsonDoc['hits']['hits']:
-			row="PH01~PH02~PH03~PH04~PH05~PH06~PH07~PH08~PH09~PH10~PH11~PH12~PH13"
+			row="PH01~PH02~PH03~PH04~PH05~PH06~PH07"
 			for key,val in  obj['fields'].iteritems():
 				if  'ITEMPAYERSHARE'==key:
-					row=row.replace('PH08',val[0])
-				#if
-				elif  'STATUS'==key:
-					row=row.replace('PH02',val[0])
-				#if
-				elif  'PROVIDER'==key:
-					row=row.replace('PH03',val[0])
-				#if
-				elif  'NETWORKTYPE'==key:
 					row=row.replace('PH04',val[0])
 				#if
-				elif  'BENEFICIARY'==key:
-					row=row.replace('PH05',val[0])
+				elif  'PROVIDER'==key:
+					row=row.replace('PH01',val[0])
+				#if
+				elif  'PROFESSIONAL'==key:
+					row=row.replace('PH02',val[0])
+				#if
+				elif  'SPECASSESSMENT'==key:
+					row=row.replace('PH03',standardizeSpecAssesment(val[0]))
+				#if
+				elif  'ITEM'==key:
+					row=row.replace('PH05',val[0].upper())
 				#if
 				elif  'TREATMENTDATE'==key:
 					row=row.replace('PH06',val[0])
 				#if
-				elif  'PROFESSIONAL'==key:
-					row=row.replace('PH07',val[0])
-				#if
-				elif  'ITEM'==key:
-					row=row.replace('PH01',val[0])
-				#if
 				elif  'CHRONIC'==key:
-					row=row.replace('PH09',val[0])
-				#if
-				elif  'SPECASSESSMENT'==key:
-					row=row.replace('PH10',val[0])
-				#if
-				elif  'SERVICE'==key:
-					row=row.replace('PH11',val[0])
-				#if
-				elif  'MASTERCONTRACT'==key:
-					row=row.replace('PH12',val[0])
-				#if
-				elif  'CLAIMTYPE'==key:
-					row=row.replace('PH13',val[0])
+					row=row.replace('PH07',val[0])
 				#if
 			#for
 			file.write(str(row.encode('utf-8').strip())+'\n')
@@ -106,7 +108,7 @@ def  loadNas_Details_Data4mElasticSearch():
 def normalizeNasData():
 	try:
 		disease_lst = [line.strip().lower() for line in open('/opt/takaful/processed-data/concerned-disease-list.csv','r')]		
-		inputDF = pd.read_csv('/opt/takaful/processed-data/pattern3-input.csv',delimiter='~')
+		inputDF = pd.read_csv('/opt/takaful/processed-data/pattern3-nas-input.csv',delimiter='~')
 		
 		def vlidateSpecAssesment(val):
 			try:
@@ -122,40 +124,19 @@ def normalizeNasData():
 				traceback.print_exc()
 			#except
 		#def
-		inputDF['SPECASSESSMENT'] = inputDF.apply(lambda row : vlidateSpecAssesment(row['SPECASSESSMENT'].lower()),axis=1)		
-		inputDF = inputDF.drop(inputDF[inputDF['SPECASSESSMENT'].str.contains('NONE')].index)
+		#inputDF['SPECASSESSMENT'] = inputDF.apply(lambda row : vlidateSpecAssesment(row['SPECASSESSMENT'].lower()),axis=1)		
+		inputDF.dropna(subset=['PROVIDER','PROFESSIONAL','SPECASSESSMENT','ITEM'],inplace=True)
 		
-		def vlidateItems(val):
-			try:
-				doc = nlp(val.decode('unicode-escape'))
-				lst =[]
-				for ent in doc.ents:
-					if ent.label_=='ORG' and str(ent)!='MG' and str(ent) !='TUBE' and str(ent)!='IU' and str(ent)!='DOSE' and str(ent)!='SACHET' and str(ent)!='STRIP' and str(ent)!='ORAL' and str(ent)!='MG 3\'S' and str(ent)!='BOTTLE' and str(ent)!='UNIT' and str(ent)!='MCG' and str(ent)!='VIAL' and str(ent) !='MG 30\'S':
-						#print ent,ent.label_
-						lst.append(str(ent))
-					#if	
-				#for
-				returnvar =  " ".join(lst) if len(lst)>=1 else "NONE"
-				if returnvar=='NONE':
-					index = re.search('\d',val)
-					if index:
-						returnvar = val[:index.start()].strip()
-					#if
-					elif '(' in val:
-						returnvar = val[:val.index('(')].strip()
-					#else
-				#if
-				
-				return returnvar
-			#try
-			except:
-				traceback.print_exc()
-			#except
-		#def
-		inputDF['ITEM'] = inputDF.apply(lambda row : vlidateItems(row['ITEM']),axis=1)		
-		#print inputDF.shape
-		inputDF.to_csv('/opt/takaful/processed-data/pattern3-normalized.csv',header=True,index=False,index_label=False)
-
+		inputDF = inputDF.drop(inputDF[inputDF['SPECASSESSMENT'].str.contains('NONE')].index)
+		inputDF['ITEM'] = inputDF.apply(lambda row : str(row['ITEM']).strip(),axis=1)		
+		
+		#report 1 = 'PROVIDER','PROFESSIONAL','SPECASSESSMENT','ITEM'
+		
+		inputDF = inputDF.groupby(['PROVIDER','PROFESSIONAL','SPECASSESSMENT','ITEM']).agg({'ITEMPAYERSHARE':[np.sum,np.mean,np.max,np.min],'ITEM':np.size}).reset_index().rename(columns={'sum':'sum_finalamt','mean':'mean_finalamt','amin':'min_finalamt','amax':'max_finalamt','size':'occurance'})
+		inputDF.columns =['PROVIDERNAME','ATTENDINGDOCTIRNAME','ICDDESCRIPTION','SERVICEDESCRIPTION','occurance','sum_finalamt','mean_finalamt','max_finalamt','min_finalamt']
+		inputDF['PROVIDERGROUP']=''
+		
+		inputDF.to_csv('/opt/takaful/processed-data/pattern3-nas-report1.csv',header=True,index=False,index_label=False)
 	#try
 	except:
 		traceback.print_exc()
@@ -292,5 +273,8 @@ if __name__=='__main__':
 	#if
 	if sys.argv[1]=='4':
 		normalizeAafiaData()
+	#if
+	if sys.argv[1]=='test':
+		print standardizeSpecAssesment('K58.9 Irritable bowel syndrome without diarrhea')
 	#if
 #if
